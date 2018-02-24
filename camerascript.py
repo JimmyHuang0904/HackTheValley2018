3a1580ff40550be97b68a27117dbd3b51d1ce2a2
Jimmy Huang <jimmyhuang0904@hotmail.com>
2018-02-24 02:16:05 -0800

    Camera python code. Need to add QR scanner to it

---
+++ camerascript.py
@@ -0,0 +1,32 @@
+# --------------------------------------------------------
+# Camera sample code for Tegra X2/X1
+#
+# This program could capture and display video from
+# IP CAM, USB webcam, or the Tegra onboard camera.
+# Refer to the following blog post for how to set up
+# and run the code:
+#   https://jkjung-avt.github.io/tx2-camera-with-python/
+#
+# Written by JK Jung <jkjung13@gmail.com>
+# --------------------------------------------------------
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(1)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
